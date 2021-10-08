4d6a0ba146f56ab55d028387fea292bca7e4d033
opilane <opilane@HP-CZC6297BB4>
2021-10-08 15:52:26 +0300

    hello

---
+++ kordamine/Ex.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kordamine
+{
+    class Ex
+    {
+        public static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int N = rnd.Next(2, 8);
+            int M = rnd.Next(9, 15);
+            int[] number = new int [M-N];
+            for (int i = N; i < M+1; i++)
+            {
+
+
+                number[i-N] = i;
+                Console.WriteLine(" {0} ", i * i);
+            }
+            foreach (var m in number)
+            {
+                Console.WriteLine("{0}", m);
+            }
+
+            Console.WriteLine("---------------------------");
+            /*
+            Console.WriteLine("Напиши первое число:");
+            double one = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Напиши второе число:");
+            double two = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Напиши третье число:");
+            double three = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Напиши четвёртое число:");
+            double four = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Напиши пятое число:");
+            double five = Convert.ToDouble(Console.ReadLine());*/
+
+            
+            
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ kordamine/korduslaused.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kordamine
+{
+    class korduslaused
+    {
+        public static void Main(string[] args)
+        {
+            string[] nimed= new string[7] {"Artem","Artjom","Alexy","Jaan","Nikita","Aljona","Nikolaj"};
+            nimed[2] = "Anna";
+            int nr = 0;
+            while (nr<7)
+            {
+                nr++;
+                Console.WriteLine($"Tere, {nr} õpilane. Sinu nimi on {nimed[nr - 1]}");
+            }
+            Console.WriteLine("-------------------------------------");
+            for (int i = 0; i < nimed.Length; i++)
+            {
+                Console.WriteLine("Tere, {0} õpilane", nimed[i]);
+            }
+            Console.WriteLine("-------------------------------------");
+            foreach (var nimi in nimed)
+            {
+                Console.WriteLine("Tere, {0} õpilane", nimi);
+            }
+            Console.WriteLine("-------------------------------------");
+            nr = 0;
+            do
+            {
+                Console.WriteLine("Tere, {0} õpilane", nimed[nr]);
+                nr++;
+            } while (nr!=nimed.Length);
+
+            Console.ReadLine();
+        }
+    }
+}
